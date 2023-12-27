15becbbe031d3f5569b15ecb6baec6ebeeb153b2
Maskymilian Mazurkiewicz <maksymilian.mazurkiewicz@surveily.com>
2023-12-27 15:30:49 +0100

    Battle tested dotfiles

---
+++ heavy_installer.sh
@@ -0,0 +1,9 @@
+sudo apt update && sudo apt upgrade
+
+# Lazydocker
+LAZYDOCKER_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazydocker/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+curl -Lo lazydocker.tar.gz "https://github.com/jesseduffield/lazydocker/releases/latest/download/lazydocker_${LAZYDOCKER_VERSION}_Linux_x86_64.tar.gz"
+mkdir lazydocker-temp
+sudo mv lazydocker-temp/lazydocker /usr/local/bin
+lazydocker --version
+rm -rf lazydocker.tar.gz lazydocker-temp
--- installer.sh
+++ installer.sh
@@ -7,26 +7,26 @@ sudo apt update && sudo apt upgrade
 
 sudo add-apt-repository ppa:aslatter/ppa
 
-# General 
+# General
 sudo apt -y install \
     zsh \
     tmux \
     alacritty
 
-cp -f .zshrc ~/.zshrc
-cp -f .p10k.zsh ~/.p10k.zsh
+pip install thefuck
+
+cp -f .zsh/.zshrc ~/.zshrc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cp -f .zsh/.p10k.zsh ~/.p10k.zsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp -f .tmux.conf ~/.tmux.conf
-cp -f alacritty.yml ~/.config/alacritty/alacritty.yml
+
+mkdir -p ~/.config/alacritty/
+cp -f .terminal/alacritty.yml ~/.config/alacritty/alacritty.yml
+
 echo -e "${RED}Note:${NC} Move .bashrc on your own! (or just add exec zsh at the end!)"
-
-# Lazydocker <3  
-LAZYDOCKER_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazydocker/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
-curl -Lo lazydocker.tar.gz "https://github.com/jesseduffield/lazydocker/releases/latest/download/lazydocker_${LAZYDOCKER_VERSION}_Linux_x86_64.tar.gz"
-mkdir lazydocker-temp
-sudo mv lazydocker-temp/lazydocker /usr/local/bin
-lazydocker --version
-rm -rf lazydocker.tar.gz lazydocker-temp
-
-
 echo -e "${GREEN}Installation complete.${NC} Make sure to customize your .bashrc file manually."
 echo -e "You may want to restart your terminal to apply the changes."
